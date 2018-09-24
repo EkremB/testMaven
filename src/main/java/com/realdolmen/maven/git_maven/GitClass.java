de3adc9e4fe091901bba75b6b90e38a72e93ef63
EkremB <43533561+EkremB@users.noreply.github.com>
2018-09-24 10:49:06 +0200

    Initial commit

---
+++ src/main/java/com/realdolmen/maven/git_maven/GitClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.maven.git_maven;
+
+/**
+ *
+ * @author ABABG10
+ */
+public class GitClass {
+    public void test(){
+        System.out.println("Hello git world");       
+    }
+    
+}
